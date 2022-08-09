06c0a2d9c82a7a493cfa690fce3db16c6dccf72c
salim <salimmattoussi@gmail.com>
2022-08-09 19:08:04 +0100

    first commit

---
+++ src/service/service.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author panda
+ */
+public interface service <T>{
+        void add(T t);
+        void delete(T t);
+        T readById(int id);
+        ArrayList<T>readAll();
+        void update(T t);
+    
+}
