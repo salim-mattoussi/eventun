70a4f623b1988937c1da5b07d60089310257338f
Mohamed <mohamed.benaoun95@gmail.com>
2022-08-10 14:18:57 +0100

    ajouter gestion evenement

---
+++ src/GestionEvenement/Evenement.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GestionEvenement;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class Evenement {
+    private int id;
+    private String nom;
+    private String lieu;
+    private String date;
+    private String description;
+
+    public Evenement(int id, String nom, String lieu, String date, String description) {
+        this.id = id;
+        this.nom = nom;
+        this.lieu = lieu;
+        this.date = date;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Evenement other = (Evenement) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nom, other.nom)) {
+            return false;
+        }
+        if (!Objects.equals(this.lieu, other.lieu)) {
+            return false;
+        }
+        if (!Objects.equals(this.date, other.date)) {
+            return false;
+        }
+        return Objects.equals(this.description, other.description);
+    }
+
+    @Override
+    public String toString() {
+        return "Evenement{" + "id=" + id + ", nom=" + nom + ", lieu=" + lieu + ", date=" + date + ", description=" + description + '}';
+    }
+  
+}
